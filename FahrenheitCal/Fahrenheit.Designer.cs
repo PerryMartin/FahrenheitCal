88dabccfda52b798c25f806df75d93d48a44bc19
PerryMartin <perry.martin@ocsbstudent.ca>
2015-10-26 14:32:42 -0400

    v1.0

---
+++ FahrenheitCal/Fahrenheit.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace FahrenheitCal
+{
+    partial class frmFahrenheit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.txtCelsius = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Location = new System.Drawing.Point(13, 13);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(141, 13);
+            this.lblQuestion.TabIndex = 0;
+            this.lblQuestion.Text = "Enter the tempture in Celsius";
+            // 
+            // txtCelsius
+            // 
+            this.txtCelsius.Location = new System.Drawing.Point(13, 48);
+            this.txtCelsius.Name = "txtCelsius";
+            this.txtCelsius.Size = new System.Drawing.Size(100, 20);
+            this.txtCelsius.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(13, 120);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(242, 23);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // frmFahrenheit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 179);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtCelsius);
+            this.Controls.Add(this.lblQuestion);
+            this.Name = "frmFahrenheit";
+            this.Text = "Fahrenheit Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.TextBox txtCelsius;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ FahrenheitCal/Fahrenheit.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Created by: Perry Martin
+ * Created on: 26-102015
+ * Created for: ICS3U
+ * Daily Assignment – Unit#4-01
+ * This program displays celsius converting to fahrenhait
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FahrenheitCal
+{
+    public partial class frmFahrenheit : Form
+
+    {
+        public void Converter(double temp)
+         {
+            double fahrenheit;
+
+            fahrenheit = (9.0/ 5.0) * temp + 32; 
+             MessageBox.Show(Convert.ToString(fahrenheit) + " degrees is the temperature in fahrenheit"); 
+         }
+
+    public frmFahrenheit()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            Double degrees;
+
+            degrees = Convert.ToDouble(this.txtCelsius.Text);
+            Converter(degrees);
+        }
+    }
+}
